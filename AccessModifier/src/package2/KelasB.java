1e23532df86d187d496e62da6a97eca784c36164
Alfian Satria Vivaldi Payu <masfian77@gmail.com>
2019-08-19 16:45:22 +0700

    Add files via upload

---
+++ AccessModifier/src/package2/KelasB.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.KelasA;
+
+public class KelasB extends KelasA {
+
+    @Override
+    protected void methodC() {
+        super.methodC();
+        System.out.println("Contoh pemanggilan protected dari package luar");
+    }
+
+}
